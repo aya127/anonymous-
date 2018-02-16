c4b6289aa5978b2a99a8e6ca9bc54b5e861f686a
Rand <rand.tarek@gmail.com>
2018-02-16 20:16:52 +0200

    .

--- DBApp.java
+++ DBApp.java
@@ -3,29 +3,41 @@ import java.util.Hashtable;
 
 
 
-public class DBApp {
- //String strClusteringKeyColumn;
- ArrayList<Table> tables;
-	public void createTable(String strTableName, String strClusteringKeyColumn,  Hashtable<String,String> htblColNameType )throws DBAppException {
-		for(int i=0; i<tables.size();i++){
-			if(tables.get(i).getStrTableName().equals(strTableName)){
-				throw new DBAppException("The table name "+strTableName+ " already exists");			
-			}	
+public class DBApp
+{
+	//String strClusteringKeyColumn;
+	ArrayList<Table> tables;
+	
+	public void createTable(String strTableName, String strClusteringKeyColumn,
+			Hashtable<String, String> htblColNameType) throws DBAppException
+	{
+		for(int i = 0; i < tables.size(); i++)
+		{
+			if(tables.get(i).getStrTableName().equals(strTableName))
+			{
+				throw new DBAppException("The table name " + strTableName + " already exists.");			
+			}
 		}
-		Table table=new Table(strTableName,strClusteringKeyColumn,htblColNameType);
-		tables.add(table);
 		
-		}
+		Table table = new Table(strTableName, strClusteringKeyColumn, htblColNameType);
+		tables.add(table);
+	}
 	
 
-	public void insertIntoTable(String strTableName,Hashtable<String,Object>  htblColNameValue)throws DBAppException {
-		for(int i=0;i<tables.size();i++){
-			if((tables.get(i).getStrTableName().equals(strTableName))){
+	public void insertIntoTable(String strTableName, Hashtable<String, Object> htblColNameValue)
+			throws DBAppException
+	{
+		for(int i = 0; i < tables.size(); i++)
+		{
+			if ((tables.get(i).getStrTableName().equals(strTableName)))
+			{
 				tables.get(i).insertIntoTable(htblColNameValue);
 				break;
 			}
-			if(!(tables.get(i).getStrTableName().equals(strTableName)) && i==tables.size()-1){
-				throw new DBAppException("The table "+strTableName+ " does not exist");
+			
+			if (!(tables.get(i).getStrTableName().equals(strTableName)) && i == tables.size()-1)
+			{
+				throw new DBAppException("The table " + strTableName + " does not exist.");
 			}
 		}
 	}
